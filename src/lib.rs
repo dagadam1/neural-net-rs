c781b140af859c85e212865ea546d6254a608ba3
Adam Olsson <dagadamsven@gmail.com>
2022-11-06 14:14:33 +0100

    Randomly initialize weights

--- src/lib.rs
+++ src/lib.rs
@@ -6,7 +6,7 @@ struct Network {
     num_layers: usize,
     sizes: Vec<usize>,
     biases: Vec<Array2<f64>>,
-    weights: Array2<f64>,
+    weights: Vec<Array2<f64>>,
 }
 
 impl Network {
@@ -16,7 +16,7 @@ impl Network {
 
         // Every Array in this vector ('biases') corresponds to a layer in the network and contains the biases for the neurons in that layer.
         // The first layer is the input layer and does not have any biases.
-        // For example: The bias of the first neuron in the second layer (first layer after the input layer) is biases[0][(0, 0)] 
+        // For example: The bias of the first neuron in the second layer (first layer after the input layer) is biases[0][(0, 0)]
         let mut biases: Vec<Array2<f64>> = vec![];
         // For every layer in the network except the first one:
         for layer_size in &sizes[1..] {
@@ -27,11 +27,18 @@ impl Network {
             );
         }
 
+        let mut weights: Vec<Array2<f64>> = vec![];
+        for (x, y) in sizes[..sizes.len() - 1].iter().zip(&sizes[1..]) {
+            weights.push(
+                Array::from_shape_fn((*y, *x), |_| rng.gen())
+            );
+        }
+
         Network{
             num_layers: sizes.len(), 
             sizes,
             biases,
-            weights: ,
+            weights,
         }
     }
 }
