ccd8600fc3f8f2d937e80731259d9bab97e46a74
Adam Olsson <dagadamsven@gmail.com>
2022-11-06 01:11:08 +0100

    Add comment

--- src/lib.rs
+++ src/lib.rs
@@ -16,6 +16,7 @@ impl Network {
 
         // Every Array in this vector ('biases') corresponds to a layer in the network and contains the biases for the neurons in that layer.
         // The first layer is the input layer and does not have any biases.
+        // For example: The bias of the first neuron in the second layer (first layer after the input layer) is biases[0][(0, 0)] 
         let mut biases: Vec<Array2<f64>> = vec![];
         // For every layer in the network except the first one:
         for layer_size in &sizes[1..] {
@@ -38,4 +39,5 @@ impl Network {
 pub fn run() {
     let test = Network::new(vec![2,3,4]);
     println!("{:?}", test);
+    println!("{:?}", test.biases[0][(0, 0)]);
 }
